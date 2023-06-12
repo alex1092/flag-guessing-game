3669ad6f8b0e309ba9a8cd2fb3c856f895c3b94c
Alex Little <alex.little@neds.com>
2023-06-12 14:41:20 +0700

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Alex Little on 12/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
